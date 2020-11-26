d851727e7d5bd013860a138af37c95a168684baf
Mrudul <bmrudul@pkglobal.com>
2020-11-26 20:12:36 +0530

    Kafka publisher integration.

--- src/main/java/com/prokarma/publisher/kafkapublisher/KafkaPublisher.java
+++ src/main/java/com/prokarma/publisher/kafkapublisher/KafkaPublisher.java
@@ -1,11 +1,15 @@
 package com.prokarma.publisher.kafkapublisher;
 
+import com.prokarma.publisher.exception.GenericException;
 import com.prokarma.publisher.model.kafkamodel.CustomerRequestKafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 public class KafkaPublisher {
@@ -24,6 +28,23 @@ public class KafkaPublisher {
      * @param customerRequestKafka
      */
     public void publish(CustomerRequestKafka customerRequestKafka) {
+        logger.info("#### -> Producing message -> ");
 
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(TOPIC, customerRequestKafka);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                logger.info("Sent message=["  +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                logger.info("Unable to send message=["
+                        + "] due to : " + ex.getMessage());
+
+                throw new GenericException("Error while publishing message to Kafka. "+ex.getMessage());
+            }
+        });
     }
 }
